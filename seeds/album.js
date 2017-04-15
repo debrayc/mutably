069a0fc17aac490dcd1068d11916e68ee7b4bd93
jlopker <jlopker@gmail.com>
2017-04-14 17:38:57 -0700

    more album data

--- seeds/album.js
+++ seeds/album.js
@@ -22,5 +22,17 @@ module.exports = [
     name: 'Dark Night of the Soul',
     releaseDate: '2008, September 12',
     genres: [ 'piano' ]
+  },
+  {
+    artistName: 'Anderson .Paak',
+    name: 'Malibu',
+    releaseDate: '2016, January 15',
+    genres: [ 'hip hop', 'neo soul' ]
+  },
+  {
+    artistName: 'Noname',
+    name: 'Telefone',
+    releaseDate: '2016, July 31',
+    genres: [ 'hip hop' ]
   }
 ];
